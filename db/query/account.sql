56d0a9e01582efbd61a4e8ef2fa0da2b022c682d
Evan Jensen <jensen55@uw.edu>
2021-07-05 21:42:01 -0700

    crud operations

--- db/query/account.sql
+++ db/query/account.sql
@@ -6,10 +6,20 @@ INSERT INTO accounts (
 )
 RETURNING *;
 
--- name: GetAuthor :one
-SELECT * FROM authors
+-- name: GetAccount :one
+SELECT * FROM accounts
 WHERE id = $1 LIMIT 1;
 
--- name: ListAuthors :many
-SELECT * FROM authors
-ORDER BY name;
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1 
+OFFSET $2;
+
+-- name: UpdateAccount :one
+UPDATE accounts SET balance = $2
+WHERE id = $1
+RETURNING *; 
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts WHERE id = $1;
